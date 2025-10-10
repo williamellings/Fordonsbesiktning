7532d9be3add46dc26f645ce481d5cb628e9f57c
williamellings <wille.ellings@hotmail.com>
2025-10-10 09:56:40 +0200

    Fixing

--- Vehicle.cs
+++ Vehicle.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.ConstrainedExecution;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fordonsbesiktning
 {
@@ -14,26 +9,28 @@ namespace Fordonsbesiktning
 
         public void CheckInspection()
         {
-            Console.WriteLine("Hur gammal är din bil?");
+            Console.Write("Hur gammal är din bil? ");
             VehicleAge = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Har bilen försäkring (true/false)? ");
-            string insuranceInput = Console.ReadLine(); 
-            HasInsurance = Convert.ToBoolean(insuranceInput); //konvertar bool
+            string insuranceInput = Console.ReadLine();
+            HasInsurance = Convert.ToBoolean(insuranceInput);
 
-
-
-            if (VehicleAge < 5 && HasInsurance) //if else, under 5 år och kolla om den har insurance med bool true eller false
+            if (VehicleAge < 5 && HasInsurance)
             {
                 Console.WriteLine("Godkänd");
             }
-            else if (VehicleAge < 5 && !HasInsurance) //om yngre och har insurance
+            else if (VehicleAge < 5 && !HasInsurance)
             {
-                Console.WriteLine("Ej godkänd måste ordna försäkring");
+                Console.WriteLine("Ej godkänd — du måste ordna försäkring.");
             }
-            else if (VehicleAge >= 5) // annars fix it
+            else if (VehicleAge >= 5 && HasInsurance)
             {
-                Console.WriteLine("Måste kompletteras");
+                Console.WriteLine("Måste kompletteras, bilen är äldre än 5 år.");
+            }
+            else if (VehicleAge >= 5 && !HasInsurance)
+            {
+                Console.WriteLine("Ej godkänd — måste ordna försäkring och komplettera besiktning.");
             }
         }
     }
